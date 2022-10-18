b1710420942edfe50f36047d90d150ac034c61b7
Laseruss <loden.micke@gmail.com>
2022-10-18 22:12:16 +0200

    changes

--- go/trie/main.go
+++ go/trie/main.go
@@ -37,6 +37,32 @@ func (t *Trie) Insert(word string) {
 	node.children = map[rune]*Trie{'*': nil}
 }
 
+func collectWords(trieNode *Trie, word string, words []string) []string {
+	currNode := trieNode
+	for key, value := range currNode.children {
+		if key == '*' {
+			words = append(words, word)
+		} else {
+			word += string(key)
+			words = collectWords(value, word, words)
+			word = word[:len(word)-1]
+		}
+	}
+	return words
+}
+
+func (t *Trie) FindCompletions(prefix string) []string {
+	currNode := t.Search(prefix)
+
+	if currNode == nil {
+		return []string{}
+	}
+
+	words := collectWords(currNode, prefix, []string{})
+
+	return words
+}
+
 func main() {
 	trie := Trie{map[rune]*Trie{}}
 	words := []string{"ace", "act", "bad", "bake", "bat", "batter", "cab", "cat", "catnap", "catnip"}
@@ -44,5 +70,5 @@ func main() {
 		trie.Insert(word)
 	}
 
-	fmt.Println(trie.Search("catn"))
+	fmt.Println(trie.FindCompletions("cat"))
 }
